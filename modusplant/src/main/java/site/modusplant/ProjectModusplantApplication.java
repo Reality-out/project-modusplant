353dd690b70e97607beb0ecb30994c652826724e
JunHyeok <akdnjs0308@gmail.com>
2025-01-02 12:19:53 +0900

    25.01.02 set the initial files for the team project

---
+++ modusplant/src/main/java/site/modusplant/ProjectModusplantApplication.java
@@ -0,0 +1,13 @@
+package site.modusplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModusplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectModusplantApplication.class, args);
+	}
+
+}
